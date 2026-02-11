083a602f2db014f34565461b145fa3d9848e1340
whitekotan0 <gcat5770@gmail.com>
2026-02-11 19:35:56 +0200

    feat: add game window with game loop and option to close

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pygame, sys
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+pygame.init()
+
+BACKGROUND = (211, 25, 11)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+WINDOW_WIDTH = 400
+WINDOW_HEIGHT = 400
+
+WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+
+def main():
+    looping = True
+
+    while looping:
+       for event in pygame.event.get():
+           if event.type == pygame.QUIT:
+               looping = False
+
+               
+       WINDOW.fill(BACKGROUND)
+       pygame.display.update()
+       
+    
+    pygame.quit()
+    sys.exit()
+
+
+
+
+
+
+main()
