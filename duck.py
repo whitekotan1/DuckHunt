e67e54a16b0e9cb99fc2b509d1cf273515b41606
Ivan Koshelia <koshelia13112006@gmail.com>
2026-02-20 14:11:08 +0200

    add music on start

--- duck.py
+++ duck.py
@@ -1,26 +1,33 @@
 import pygame
 import random
 
+
 class Duck:
     def __init__(self, screen_width, screen_height):
 
         self.width = screen_width
         self.height = screen_height
 
+
         self.x = random.randint(50, screen_width - 50)
         self.y = random.randint(50, screen_height - 50)
 
-        self.speed_x = random.randint(-5, 5) or 1
-        self.speed_y = random.randint(-5, 5) or 1
 
-        self.img = pygame.image.load('design\duck-duckhunt.png')
+        self.speed_x = random.randint(-5, 5) or 2
+        self.speed_y = random.randint(-5, 5) or 2
+
+
+        self.img = pygame.image.load("design/duck-duckhunt.png")
         self.img = pygame.transform.scale(self.img, (50, 50))
+
         self.alive = True
 
     def update(self):
+
         self.x += self.speed_x
         self.y += self.speed_y
-        
+
+
         if self.x < 0 or self.x > self.width - 50:
             self.speed_x = -self.speed_x
         if self.y < 0 or self.y > self.height - 50:
@@ -29,10 +36,6 @@ class Duck:
     def draw(self, screen):
         screen.blit(self.img, (int(self.x), int(self.y)))
 
-
     def dead(self):
-         self.alive = False
-         return self.alive
 
-
-    
+        self.alive = False
--- main.py
+++ main.py
@@ -4,28 +4,36 @@ from gun import Gun
 from design import Design
 from duck import Duck
 
-
 pygame.init()
+pygame.mixer.init()
 
 
 WINDOW_WIDTH = 400
 WINDOW_HEIGHT = 400
-BACKGROUND = (211, 25, 11)
-
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-design = Design(WINDOW_WIDTH, WINDOW_HEIGHT)
 pygame.display.set_caption("Duck Hunt")
 
 clock = pygame.time.Clock()
 
+design = Design(WINDOW_WIDTH, WINDOW_HEIGHT)
 
 gun = Gun()
-duck =Duck(WINDOW_WIDTH,WINDOW_HEIGHT)
+duck = None
+
+DUCK_SPAWN_DELAY = 5000
+last_duck_spawn_time = pygame.time.get_ticks()
+
+
+start_sound = pygame.mixer.Sound("assets/sounds/start_game.wav")
+start_sound.set_volume(0.8)
+
 
 state = "menu"
 
+
 font_big = pygame.font.SysFont(None, 36)
 font_small = pygame.font.SysFont(None, 28)
+
 
 button_rect = pygame.Rect(
     WINDOW_WIDTH // 2 - 50,
@@ -36,7 +44,7 @@ button_rect = pygame.Rect(
 
 
 def main():
-    global state
+    global state, duck, last_duck_spawn_time
     running = True
 
     while running:
@@ -45,22 +53,25 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
-
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1: # лкм
+                if event.button == 1:
 
 
                     if state == "menu" and button_rect.collidepoint(event.pos):
+                        start_sound.play()            # звук початку гри
                         state = "game"
+                        duck = None                      # утка з'явиться пізніше
+                        last_duck_spawn_time = pygame.time.get_ticks()
 
 
                     elif state == "game":
                         gun.shoot(event.pos)
 
+
         design.draw(WINDOW)
 
-        if state == "menu":
 
+        if state == "menu":
             text = font_big.render(
                 "Ready to start the game",
                 True,
@@ -70,7 +81,6 @@ def main():
                 text,
                 (WINDOW_WIDTH // 2 - text.get_width() // 2, 120)
             )
-
 
             pygame.draw.rect(WINDOW, (0, 200, 0), button_rect)
             go_text = font_small.render("GO", True, (0, 0, 0))
@@ -82,10 +92,19 @@ def main():
                 )
             )
 
+
         elif state == "game":
-            if duck.alive == True:
-                duck.update()  
-                duck.draw(WINDOW)  
+            current_time = pygame.time.get_ticks()
+
+
+            if duck is None:
+                if current_time - last_duck_spawn_time >= DUCK_SPAWN_DELAY:
+                    duck = Duck(WINDOW_WIDTH, WINDOW_HEIGHT)
+
+            else:
+                if duck.alive:
+                    duck.update()
+                    duck.draw(WINDOW)
 
             gun.update()
             gun.draw(WINDOW)
